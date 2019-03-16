12ae4e7d3f54f1450509fe328edc0c0241b4f62d
Zoltan Altfatter <altfatterz@gmail.com>
2019-03-16 22:46:57 +0100

    initial commit

---
+++ customer-service/src/main/java/com/example/CacheMetricsRegistration.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import org.springframework.boot.actuate.metrics.cache.CacheMetricsRegistrar;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * https://stackoverflow.com/questions/49697063/after-upgrade-to-spring-boot-2-how-to-expose-cache-metrics-to-prometheus
+ */
+
+@Component
+@Profile("prod")
+public class CacheMetricsRegistration {
+
+    private final CacheMetricsRegistrar cacheMetricsRegistrar;
+
+    private final CacheManager cacheManager;
+
+    public CacheMetricsRegistration(CacheMetricsRegistrar cacheMetricsRegistrar, CacheManager cacheManager) {
+        this.cacheMetricsRegistrar = cacheMetricsRegistrar;
+        this.cacheManager = cacheManager;
+    }
+
+    @EventListener(ApplicationReadyEvent.class) // can this be delayed until the cache is created dynamically?
+    public void register() {
+        Cache countries = this.cacheManager.getCache("countries");
+        this.cacheMetricsRegistrar.bindCacheToRegistry(countries);
+    }
+}
---
+++ customer-service/src/main/java/com/example/CustomerServiceApplication.java
@@ -0,0 +1,59 @@
+package com.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.Serializable;
+
+@SpringBootApplication
+@EnableCaching
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+}
+
+@RestController
+class CustomerRestController {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerRestController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @GetMapping(value = "/customers", params = "id")
+    public Customer findByCustomerId(@RequestParam String id) {
+        return customerRepository.findCustomerById(id);
+    }
+}
+
+@Component
+@CacheConfig(cacheNames = "countries")
+class CustomerRepository {
+
+    @Cacheable
+    public Customer findCustomerById(String id) {
+        System.out.println("Loading customer with id " + id);
+        return new Customer(id);
+    }
+
+}
+
+@Data
+@AllArgsConstructor
+class Customer implements Serializable {
+
+    private String id;
+}
