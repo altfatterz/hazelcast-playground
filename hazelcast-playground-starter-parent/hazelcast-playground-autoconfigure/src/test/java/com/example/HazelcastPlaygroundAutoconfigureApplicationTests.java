566ad4525f88549c01ef65a87e6cf7b2d9c6d10e
Zoltan Altfatter <altfatterz@gmail.com>
2019-06-18 18:18:52 +0200

    add initial starter

--- hazelcast-playground-starter-parent/hazelcast-playground-autoconfigure/src/test/java/com/example/HazelcastPlaygroundAutoconfigureApplicationTests.java
+++ hazelcast-playground-starter-parent/hazelcast-playground-autoconfigure/src/test/java/com/example/HazelcastPlaygroundAutoconfigureApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CustomerServiceApplicationTests {
+public class HazelcastPlaygroundAutoconfigureApplicationTests {
 
     @Test
     public void contextLoads() {
--- hazelcast-playground-starter-parent/hazelcast-playground-starter/src/test/java/com/example/HazelcastPlaygroundStarterApplicationTests.java
+++ hazelcast-playground-starter-parent/hazelcast-playground-starter/src/test/java/com/example/HazelcastPlaygroundStarterApplicationTests.java
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class OrderServiceApplicationTests {
+public class HazelcastPlaygroundStarterApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 }
